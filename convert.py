95e0b7720009aa7e3a86a490f350182a022f65fd
cxzlw <wsc_zds@qq.com>
2023-05-17 21:08:01 +0800

    normalize.

--- convert.py
+++ convert.py
@@ -1,10 +1,11 @@
 from fontTools import ttLib
+from unicodedata import normalize
 
 t = ttLib.TTFont(input("Input font filename: "))
 
 zhihu_dict = {}
 
 for x in t["cmap"].tables[0].cmap.items():
-    zhihu_dict[chr(x[0])] = chr(int(x[1][3:], 16))
+    zhihu_dict[chr(x[0])] = normalize("NFKC", chr(int(x[1][3:], 16)))
 
 print(zhihu_dict)
